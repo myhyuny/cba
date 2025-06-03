37bc49b506936927e9b79c8a7a2453e01808dce9
Hyunmin Kang <myhyuny@live.com>
2025-06-04 03:57:35 +0900

    multi thread zopfli

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,24 @@
 use clap::Parser;
 use lazy_regex::{lazy_regex, Lazy, Regex};
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use std::{
     cmp::Ordering,
+    collections::HashSet,
     ffi::OsStr,
     fs::{read_dir, rename, File},
-    io::{copy, Write},
+    io::{copy, BufReader, BufWriter, Write},
     path::PathBuf,
 };
-use zip::{
-    write::SimpleFileOptions,
-    CompressionMethod::{self, Deflated, Stored},
-    ZipWriter,
-};
+use zip::{write::SimpleFileOptions, CompressionMethod, ZipWriter};
 
-struct Extension<'n> {
-    name: &'n str,
-    method: CompressionMethod,
-    level: Option<i64>,
-}
+static NUMBER_REGEX: Lazy<Regex> = lazy_regex!(r"(\d+)");
 
-const EXTENSIONS: [Extension; 6] = [
-    Extension {
-        name: "AVIF",
-        method: Stored,
-        level: None,
-    },
-    Extension {
-        name: "GIF",
-        method: Deflated,
-        level: Some(9),
-    },
-    Extension {
-        name: "JPG",
-        method: Deflated,
-        level: Some(9),
-    },
-    Extension {
-        name: "JPEG",
-        method: Deflated,
-        level: Some(9),
-    },
-    Extension {
-        name: "PNG",
-        method: Stored,
-        level: None,
-    },
-    Extension {
-        name: "WEBP",
-        method: Stored,
-        level: None,
-    },
-];
+static EXTENSIONS: Lazy<HashSet<String>> = Lazy::new(|| {
+    ["AVIF", "GIF", "JPG", "JPEG", "PNG", "WEBP"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect()
+});
 
 #[derive(Parser)]
 struct Args {
@@ -67,119 +35,118 @@ fn main() -> Result<(), Error> {
 
     let args = Args::parse();
 
-    for path in &args.dirs {
-        let mut images = vec![];
-        for dir in read_dir(path)? {
-            let path = dir?.path();
-            if !path.is_dir()
-                && path
-                    .extension()
-                    .and_then(OsStr::to_str)
-                    .map(str::to_uppercase)
-                    .filter(|s| EXTENSIONS.iter().any(|e| e.name == s))
-                    .is_some()
-            {
-                images.push(path);
-            }
-        }
-        if images.is_empty() {
-            continue;
-        }
-
-        images.sort_by(|a, b| {
-            static REGEX: Lazy<Regex> = lazy_regex!(r"(\d+)");
-
-            if let (Some(mut ai), Some(mut bi)) = (
-                a.file_name()
-                    .and_then(OsStr::to_str)
-                    .map(|n| REGEX.captures_iter(n)),
-                b.file_name()
-                    .and_then(OsStr::to_str)
-                    .map(|n| REGEX.captures_iter(n)),
-            ) {
-                loop {
-                    if let (Some(an), Some(bn)) = (
-                        ai.next().and_then(|c| c[1].parse::<u64>().ok()),
-                        bi.next().and_then(|c| c[1].parse::<u64>().ok()),
-                    ) {
-                        let cmp = an.cmp(&bn);
-                        if cmp != Ordering::Equal {
-                            return cmp;
-                        }
-                        continue;
-                    }
-                    break;
+    args.dirs
+        .par_iter()
+        .try_for_each(|path| -> Result<(), Error> {
+            let mut images = Vec::new();
+            for dir in read_dir(path)? {
+                let path = dir?.path();
+                if !path.is_dir()
+                    && path
+                        .extension()
+                        .and_then(OsStr::to_str)
+                        .map(str::to_uppercase)
+                        .filter(|s| EXTENSIONS.contains(s))
+                        .is_some()
+                {
+                    images.push(path);
                 }
             }
+            if images.is_empty() {
+                return Ok(());
+            }
 
-            return a.cmp(b);
-        });
+            images.sort_by(|a, b| {
+                if let (Some(mut ai), Some(mut bi)) = (
+                    a.file_name()
+                        .and_then(OsStr::to_str)
+                        .map(|n| NUMBER_REGEX.captures_iter(n)),
+                    b.file_name()
+                        .and_then(OsStr::to_str)
+                        .map(|n| NUMBER_REGEX.captures_iter(n)),
+                ) {
+                    loop {
+                        if let (Some(an), Some(bn)) = (
+                            ai.next().and_then(|c| c[1].parse::<u64>().ok()),
+                            bi.next().and_then(|c| c[1].parse::<u64>().ok()),
+                        ) {
+                            let cmp = an.cmp(&bn);
+                            if cmp != Ordering::Equal {
+                                return cmp;
+                            }
+                            continue;
+                        }
+                        break;
+                    }
+                }
 
-        let names = digit(images.len());
-        let targets = (0..images.len())
-            .filter_map(|i| {
-                let ext = images[i]
-                    .extension()
-                    .and_then(OsStr::to_str)?
-                    .to_uppercase();
-                let ext = match ext.as_str() {
-                    "JPEG" => "JPG".to_owned(),
-                    _ => ext,
-                };
-                let image = path.join(format!("{}{}.{}", "0".repeat(names - digit(i)), i, ext));
-                return Some(image);
-            })
-            .collect::<Vec<_>>();
+                return a.cmp(b);
+            });
 
-        let sources = if targets.iter().find(|&p| images.contains(p)).is_none() {
-            images
-        } else {
-            let parent = path.display();
-            let targets = images
-                .iter()
-                .filter_map(|b| {
-                    let child = b.file_name().and_then(OsStr::to_str)?;
-                    let path = PathBuf::from(format!("{}/_{}", parent, child));
-                    return Some(path);
+            let names = digit(images.len());
+            let targets = (0..images.len())
+                .filter_map(|i| {
+                    let ext = images[i]
+                        .extension()
+                        .and_then(OsStr::to_str)?
+                        .to_uppercase();
+                    let ext = match ext.as_str() {
+                        "JPEG" => "JPG".to_owned(),
+                        _ => ext,
+                    };
+                    let image = path.join(format!("{}{}.{}", "0".repeat(names - digit(i)), i, ext));
+                    return Some(image);
                 })
-                .collect();
-            rename_all(&images, &targets)?;
-            targets
-        };
+                .collect::<Vec<_>>();
 
-        rename_all(&sources, &targets)?;
+            let sources = if !targets.iter().any(|p| images.contains(p)) {
+                images
+            } else {
+                let parent = path.display();
+                let targets: Vec<PathBuf> = images
+                    .iter()
+                    .filter_map(|b| {
+                        let child = b.file_name().and_then(OsStr::to_str)?;
+                        let path = PathBuf::from(format!("{}/_{}", parent, child));
+                        return Some(path);
+                    })
+                    .collect();
+                rename_all(&images, &targets)?;
+                targets
+            };
 
-        let file = File::create(format!("{}.cbz", path.display()))?;
-        let mut zip = ZipWriter::new(file);
+            rename_all(&sources, &targets)?;
 
-        for path in targets {
-            let name = path
-                .file_name()
-                .and_then(OsStr::to_str)
-                .ok_or("invalid file name")?;
-            let ext = path
-                .extension()
-                .and_then(OsStr::to_str)
-                .and_then(|s| EXTENSIONS.iter().find(|e| e.name == s))
-                .ok_or("invalid extension")?;
+            let file = File::create(format!("{}.cbz", path.display()))?;
+            let buf = BufWriter::new(file);
+            let mut zip = ZipWriter::new(buf);
 
-            let options = SimpleFileOptions::default()
-                .compression_method(ext.method)
-                .compression_level(ext.level);
+            for path in targets {
+                let name = path
+                    .file_name()
+                    .and_then(OsStr::to_str)
+                    .ok_or("invalid file name")?;
 
-            zip.start_file(name, options)?;
-            let mut file = File::open(&path)?;
-            copy(&mut file, &mut zip)?;
-            println!("{}", &name);
-        }
+                let options = SimpleFileOptions::default()
+                    .compression_method(CompressionMethod::Deflated)
+                    .compression_level(Some(15));
 
-        zip.flush()?;
-    }
+                zip.start_file(name, options)?;
+                let file = File::open(&path)?;
+                let mut reader = BufReader::new(file);
+                copy(&mut reader, &mut zip)?;
+                println!("{}", &name);
+            }
+
+            zip.flush()?;
+
+            return Ok(());
+        })?;
 
     return Ok(());
 }
 
-fn rename_all(sources: &Vec<PathBuf>, targets: &Vec<PathBuf>) -> Result<(), Error> {
+fn rename_all(sources: &[PathBuf], targets: &[PathBuf]) -> Result<(), Error> {
     for i in 0..usize::min(sources.len(), targets.len()) {
         rename(&sources[i], &targets[i])?;
     }
@@ -190,4 +157,4 @@ fn digit(i: usize) -> usize {
     return (i as f64).log10().floor() as usize + 1;
 }
 
-pub type Error = Box<dyn std::error::Error>;
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
